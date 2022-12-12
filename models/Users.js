fb3726536e471556afcaf0a69730a6c069b1f797
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-12-12 15:16:31 -0700

    added before create hook

--- models/Users.js
+++ models/Users.js
@@ -42,6 +42,12 @@ User.init(
   },
   {
     hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        console.log(newUserData.password);
+        newUserData.email = newUserData.toLowerCase();
+      },
+
       beforeBulkCreate: async (newUserData) => {
         //password hashing
         for (let index = 0; index < newUserData.length; index++) {
