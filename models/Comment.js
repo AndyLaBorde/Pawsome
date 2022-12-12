9f164638506d88664a06a43e467f3b5d793d939c
Zach Weston <zachpweston@gmail.com>
2022-12-12 10:39:39 -0700

    comment model

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            }
+        },
+        user_name: {
+            type: DataTypes.STRING,
+            allowNulll: false,
+            
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }, 
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
