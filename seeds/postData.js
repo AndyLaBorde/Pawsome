b3ca05110abf964fec67e662531024d82fc0e5b1
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-12-14 18:06:46 -0700

    added post page styling

--- seeds/postData.js
+++ seeds/postData.js
@@ -10,6 +10,42 @@ const postData = [
     //comment Id could be removed
     comments_id: 5,
   },
+  {
+    user_id: 2,
+    photo:
+      "https://drive.google.com/file/d/1uZN00KZvZ92w6_BbN5XHBoOyCaWEUYex/preview",
+    description: "Im a turtle",
+    likes: 4,
+    //comment Id could be removed
+    comments_id: 5,
+  },
+  {
+    user_id: 1,
+    photo:
+      "https://drive.google.com/file/d/1uZN00KZvZ92w6_BbN5XHBoOyCaWEUYex/preview",
+    description: "I think im actually a tortious",
+    likes: 4,
+    //comment Id could be removed
+    comments_id: 5,
+  },
+  {
+    user_id: 2,
+    photo:
+      "https://drive.google.com/file/d/1uZN00KZvZ92w6_BbN5XHBoOyCaWEUYex/preview",
+    description: "Im a cool turtle ",
+    likes: 4,
+    //comment Id could be removed
+    comments_id: 5,
+  },
+  {
+    user_id: 2,
+    photo:
+      "https://drive.google.com/file/d/1uZN00KZvZ92w6_BbN5XHBoOyCaWEUYex/preview",
+    description: "This is the same picture!",
+    likes: 4,
+    //comment Id could be removed
+    comments_id: 5,
+  },
 ];
 
 const seedPostData = () => Post.bulkCreate(postData);
