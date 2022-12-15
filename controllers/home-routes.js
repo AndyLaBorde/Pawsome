c2687e6e060fed730a372b45a1c1c16105527253
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-12-15 16:37:39 -0700

    added user page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Comment, Post } = require("../models");
+const { User, Comment, Post, Following, Follower } = require("../models");
 // TODO: Import the custom middleware
 
 // GET all posts for homepage
@@ -86,6 +86,30 @@ router.get("/comment/:id", async (req, res) => {
       res.render("comment", {
         loggedInUser,
         post,
+      });
+    } else {
+      res.redirect("/login");
+    }
+    res.status(200);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/user/:id", async (req, res) => {
+  try {
+    if (req.session.loggedIn) {
+      //get comment by id
+      data = await User.findByPk(req.params.id, {
+        include: [{ model: Post }, { model: Following }, { model: Follower }],
+      });
+      data = data.get({ plain: true });
+      console.log(data.posts[0].photo);
+      loggedInUser = req.session.user;
+      res.render("userpage", {
+        loggedInUser,
+        data,
       });
     } else {
       res.redirect("/login");
