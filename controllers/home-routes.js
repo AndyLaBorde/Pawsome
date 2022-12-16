ca3a47929a0a21c8d8c9c9e9adf86e8da5a499a1
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-12-15 19:09:50 -0700

    added log out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -144,6 +144,17 @@ router.get("/search", async (req, res) => {
   }
 });
 
+router.get("/logout", async (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+  res.redirect("/login");
+});
+
 router.get;
 
 module.exports = router;
