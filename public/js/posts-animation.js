f0d5f02ccf5a7030fcb11c2534657f00e0bc1ac4
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2022-12-15 17:25:25 -0700

    fixed like bone color change

--- public/js/posts-animation.js
+++ public/js/posts-animation.js
@@ -1,31 +1,32 @@
 $(document).ready(function () {
     var boneButton = $('#likeButton');
     var commentBubble = $('#commentButton');
-    var bone = $('#bone');
+    var bone = $('.bone');
+    // var comment = $('#comment')
 
-    boneButton.click (() => {
-        // fix animation later
-        
-        anime({
-            target: bone,
-            rotate: {
-                value: 360,
-                duration: 900,
-                easing: 'easeInOutSine'
-            },
-        });
-        
-        bone.attr('src', '../images/dog-bone-logo-red.png')
+    bone.click ((event) => {
+        var target = $(event.target)
+        console.log('bone clicked');
+        target.attr('src', '../images/dog-bone-logo-red.png')
     });
 
     commentBubble.click (() => {
-        anime({
+        this.attr('src', '../images/comment-icon-red.png')
+    });
+    anime({
             target: '#comment',
             rotate: {
-                value: 360,
-                duration: 900,
+                value: '2turn',
+                duration: 1000,
                 easing: 'easeInOutSine'
             }
-        })
-    });
+    })
+    anime({
+            target: '#bone',
+            rotate: {
+                value: '2turn',
+                duration: 1000,
+                easing: 'easeInOutSine'
+            }
+    })
 });
